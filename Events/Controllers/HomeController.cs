7694d6776decbf2777bd0ddd34944b8d7ca8f67e
lukaszDNowak <32517296+lukaszDNowak@users.noreply.github.com>
2018-05-16 19:11:15 +0200

    .

---
+++ Events/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Events.Models;
+
+namespace Events.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        public IActionResult Index()
+        {
+            ViewBag.messege = "Cześć Łukasz";
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            UserRepository.AddUser(user);
+            return View("Thanks",user);
+        }
+
+
+
+
+
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Events/Models/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Events.Models
+{
+    public static class UserRepository
+    {
+        private static List<User> users = new List<User>();
+
+        public static List<User> Users
+        {
+           get
+            {
+                return users;
+            }
+        }
+        public static void AddUser(User user)
+        {
+            users.Add(user);
+        }
+    }
+}
+
